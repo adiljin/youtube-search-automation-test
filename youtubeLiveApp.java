27e7852206f12edd96dbde94ff3ffd3283176d69
Adil Shalimbekov <adil_s19@mail.ru>
2020-05-16 17:27:07 -0700

    Adding comments

--- youtubeLiveApp.java
+++ youtubeLiveApp.java
@@ -21,6 +21,8 @@ public class youtubeLiveApp {
         cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
         cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Demo");
+
+        //Getting inside downloaded app (YouTube)
         cap.setCapability("appPackage", "com.google.android.youtube");
         cap.setCapability("appActivity", "com.google.android.youtube.HomeActivity");
 
@@ -28,22 +30,28 @@ public class youtubeLiveApp {
 
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
+        //Push "Search" button
         TouchAction t = new TouchAction(driver);
         WebElement checkBox = driver.findElementsByClassName("android.support.v7.widget.LinearLayoutCompat").get(0);  //"android.widget.ImageView[@content-desc='Search']"
         t.tap(ElementOption.element(checkBox)).perform();
 
+        //Enter "Alex 007"
         driver.findElementByClassName("android.widget.EditText").sendKeys("Alex 007");
-
+        //Push Enter button
         driver.pressKey( new KeyEvent(AndroidKey.ENTER));
 
+        //Choose desired YouTube channel
         WebElement alex = driver.findElementByAndroidUIAutomator("text(\"Alex007SC2\")");
         t.tap(ElementOption.element(alex)).perform();
 
+        //Going to "Videos" section
         WebElement videos = driver.findElementByAndroidUIAutomator("text(\"VIDEOS\")");
         t.tap(ElementOption.element(videos)).perform();
 
+        //Scrolling down and looking for specific video ("Я ТРЕБУЮ БАН: Полный разбор игр гениального террана...")
         driver.findElementByAndroidUIAutomator("UiScrollable(new UiSelector()).scrollIntoView(text(\"Я ТРЕБУЮ БАН: Полный разбор игр гениального террана из грандмастер лиги StarCraft II\"))");
 
+        //Find the video and start watching it.
         WebElement vid1 = driver.findElementByAndroidUIAutomator("text(\"Я ТРЕБУЮ БАН: Полный разбор игр гениального террана из грандмастер лиги StarCraft II\")");
         t.tap(ElementOption.element(vid1)).perform();
 
